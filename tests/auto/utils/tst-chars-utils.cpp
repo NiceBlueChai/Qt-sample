58b5b1297bad24e37e9d10ea0f9f8941b0f7a116
NiceBlueChai <bluechai@qq.com>
2022-01-19 22:21:52 +0800

    添加单元测试

---
+++ tests/auto/utils/tst-chars-utils.cpp
@@ -0,0 +1,25 @@
+﻿#ifdef _MSC_BUILD
+#pragma execution_character_set("utf-8")
+#endif
+#include <QTest>
+
+#include "chars-utils.h"
+using namespace nbc::utils;
+class tst_CharsUtils : public QObject {
+    Q_OBJECT
+
+private slots:
+
+    void tstTruncationUtf8() {
+        QByteArray ba("Hello尼赛积分虫三扥看");
+        int ret = truncationUtf8(reinterpret_cast<unsigned char*>(ba.data()), ba.length(), 10);
+        QVERIFY(ret == 8);
+
+        ret = truncationUtf8(reinterpret_cast<unsigned char*>(ba.data()), ba.length(), 5);
+        QVERIFY(ret == 5);
+    }
+};
+
+
+QTEST_MAIN(tst_CharsUtils)
+#include "tst-chars-utils.moc"
---
+++ tests/auto/utils/tst-gui-utils.cpp
@@ -0,0 +1,14 @@
+﻿#include <QTest>
+
+#include "chars-utils.h"
+
+class tst_GuiUtils : public QObject {
+    Q_OBJECT
+
+private slots:
+    void initCase(){}
+};
+
+
+QTEST_MAIN(tst_GuiUtils)
+#include "tst-gui-utils.moc"
